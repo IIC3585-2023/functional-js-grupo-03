f177fead10d16e89cae30e39fc182d154b1abc7a
Javiera Saavedra <javiera.saavedra@uc.cl>
2023-03-26 12:47:29 -0300

    feat(utils): add function to trim paragraph and to add line breaks

--- utils.js
+++ utils.js
@@ -2,3 +2,26 @@ import _ from 'lodash';
 
 const increaseSentenceIndentation = (paragraph, n) => paragraph.map(sentence => sentence.trimStart().padStart(n));
 const convertSentencesIndentations = (paragraphs, n) => paragraphs.map(paragraph => increaseSentenceIndentation(paragraph, n));
+
+const chunkLast = (paragraph) => [_.dropRight(paragraph), _.last(paragraph)]
+const addLineBreaks = ([rest, last], n) => [...rest, last.padEnd(n, '\n')]
+const convertParagraphLineBreaks = (paragraphs, n) => paragraphs.map(paragraph => addLineBreaks(chunkLast(paragraph), n));
+
+const joinParagraph = (paragraph) => paragraph.join('. ') + '.';
+const trimToN = (string, n) => string.substr(0, n);
+const trimToLastWord = (string) => string.substr(0, Math.min(string.length, string.lastIndexOf(' ')));
+const splitParagraph = (string) => string.split('.')
+                                        .filter(sentence => sentence.length > 0)
+                                        .map(sentence => sentence.trim());
+const trimParagraph = (paragraph, n) => _.flow(
+  joinParagraph,
+  trimToN.bind(this, n),
+  trimToLastWord,
+  splitParagraph
+)(paragraph)
+
+export {
+  convertSentencesIndentations,
+  convertParagraphLineBreaks,
+  trimParagraph
+}
